7f68ad9872de54932d3bf8184ef080bd2d24e2ff
VolodyaAll <volodya22all@gmail.com>
2020-08-12 18:12:18 +0300

    Change .png to .xml pictures

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,25 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.imageView)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
-            rollDice()
-        }
+
+        rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultImageView: ImageView = findViewById(R.id.imageView)
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -31,8 +28,8 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        resultImageView.setImageResource(drawableResource)
-        resultImageView.contentDescription = diceRoll.toString()
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 
 }
