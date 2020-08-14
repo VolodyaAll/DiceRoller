93e7b2425f37750c2b706bb4c38378b2d9b728b6
VolodyaAll <volodya22all@gmail.com>
2020-08-14 10:08:42 +0300

    Add second dice and clear button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,21 +6,31 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
+    lateinit var rollButton: Button
+    lateinit var clearButton: Button
+    lateinit var diceImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.imageView)
-
-        val rollButton: Button = findViewById(R.id.button)
+        diceImage1 = findViewById(R.id.imageView)
+        diceImage2 = findViewById(R.id.imageView2)
+        rollButton = findViewById(R.id.button)
+        clearButton = findViewById(R.id.button2)
 
         rollButton.setOnClickListener { rollDice() }
+        clearButton.setOnClickListener { setDefaultDiceImage() }
     }
 
-    private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val drawableResource = when (diceRoll) {
+    private fun setDefaultDiceImage() {
+        diceImage1.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val diceRoll = Dice(6).roll()
+        return when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -28,10 +38,12 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
-        diceImage.contentDescription = diceRoll.toString()
     }
 
+    private fun rollDice() {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
 }
 
 class Dice(private val numSides: Int) {
